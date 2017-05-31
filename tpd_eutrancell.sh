69e463386d453fc4d384336176dfb642c18f74f4
MZqk <zqk_mz@163.com>
2017-05-31 15:09:32 +0800

    4G性能汇总

--- tpd_eutrancell.sh
+++ tpd_eutrancell.sh
@@ -6,15 +6,14 @@ source ~/.cshrc
 
 hour=$1
 daytime=`date +%Y-%m-%d`
-{perl /opt/BOCO.NPM/DB/ReportSum/bin/Tpa_eutrancell_hour_sum.pl -c Tpa_eutrancell.cfg -s "$daytime ${hour}:00:00" -e "$daytime ${hour}:59:59"}>>tpd_eutrancell.sh.log 
+perl /opt/BOCO.NPM/DB/ReportSum/bin/Tpa_eutrancell_hour_sum.pl -c Tpa_eutrancell.cfg -s "$daytime ${hour}:00:00" -e "$daytime ${hour}:59:59">>tpd_eutrancell.sh.log 
 
 
 # 如果数据缺失发生在前一天，则第二天采集汇总后需重新进行天粒度汇总（不加时间默认p汇总前一天的数据）
 #perl $DB_PATH/ReportSum/bin/foa_tpa_td_sum_day.pl -c Tpa_eutrancell.cfg -s "$starttime" -e "$endtime"
 
 #以下为prb汇总
-{
+
 perl /opt/BOCO.NPM/DB/ReportSum/bin/Tpa_lte_hour_ne.pl -c Tpa_eutr_prb_yunnan.cfg -s "$start"  -e "$end"
-perl /opt/BOCO.NPM/DB/ReportSum/bin/Tpa_lte_hour_sum.pl -c Tpa_eutr_prb_yunnan.cfg -s "$start"  -e "$end"
-}>>prb.sh.log
+perl /opt/BOCO.NPM/DB/ReportSum/bin/Tpa_lte_hour_sum.pl -c Tpa_eutr_prb_yunnan.cfg -s "$start"  -e "$end">>prb.sh.log
 
