22bf1ddf392317def9f53c999ab8c14eaa32fe7d
MZZZ <961504168@qq.com>
2017-06-05 11:12:13 +0800

    资源表快照

--- resource.sh
+++ resource.sh
@@ -1,10 +1,10 @@
 #采集数据正常后需重新做快照
 source ~/.cshrc
 #++++脚本运行格式++++
-#sh resource.sh tbl_name1 tbl_name2
+#sh resource.sh tbl_name
 #脚本后tbl_name为所需重做的快照名
 
-tbl_name=$@
+tbl_name=$1
 
 perl /opt/BOCO.NPM/DB/snap/sync_parcm.pl -t "$tbl_name" >> resource.sh.log
 
